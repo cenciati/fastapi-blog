5fd278be959872f2757ea1d89b6a6ae371f6bb76
Gabriel Cenciati <gbrcenciati@gmail.com>
2022-12-09 10:34:36 -0300

    add: database setup

--- backend/app/infra/config/db_config.py
+++ backend/app/infra/config/db_config.py
@@ -7,19 +7,19 @@ from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv(find_dotenv())
-DB_DRIVER: str = "postgresql+psycopg2"
 DB_USER: Optional[str] = getenv("DB_USER")
 DB_PASSWORD: Optional[str] = getenv("DB_PASSWORD")
 DB_HOST: Optional[str] = getenv("DB_HOST")
-DB_PORT: Optional[str] = getenv("DB_PORT")
-DB_DATABASE_NAME: Optional[str] = getenv("DB_DATABASE_NAME")
+DB_NAME: Optional[str] = getenv("DB_NAME")
 
 
 class DBConnectionHandler:
     """SQLAlchemy database connection helper."""
 
     def __init__(self) -> None:
-        self.__connection_string = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE_NAME}"
+        self.__connection_string: str = (
+            f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+        )
         self.sesion = None
 
     def get_engine(self) -> Engine:
@@ -36,8 +36,10 @@ class DBConnectionHandler:
     def __enter__(self):
         engine = create_engine(self.__connection_string)
         session_maker = sessionmaker()
-        self.session = session_maker(bind=engine)  # pylint: disable=W0201
+        self.session = session_maker(  # pylint: disable=attribute-defined-outside-init
+            autocommit=False, autoflush=False, bind=engine
+        )
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.sesion.close()  # type: ignore
+        self.session.close()  # type: ignore
