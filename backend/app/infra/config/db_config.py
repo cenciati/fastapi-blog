1ff8b71356d287f561b452ebaa09c33911dab171
Gabriel Cenciati <gbrcenciati@gmail.com>
2022-12-10 22:33:34 -0300

    chore: move db driver and port infos to .env file

--- backend/app/infra/config/db_config.py
+++ backend/app/infra/config/db_config.py
@@ -7,9 +7,11 @@ from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv(find_dotenv())
+DB_DRIVER: Optional[str] = getenv("DB_DRIVER")
 DB_USER: Optional[str] = getenv("DB_USER")
 DB_PASSWORD: Optional[str] = getenv("DB_PASSWORD")
 DB_HOST: Optional[str] = getenv("DB_HOST")
+DB_PORT: Optional[str] = getenv("DB_PORT")
 DB_NAME: Optional[str] = getenv("DB_NAME")
 
 
@@ -18,25 +20,31 @@ class DBConnectionHandler:
 
     def __init__(self) -> None:
         self.__connection_string: str = (
-            f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+            f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
         )
+        self.__engine = self.__create_database_engine()
         self.sesion = None
 
-    def get_engine(self) -> Engine:
+    def __create_database_engine(self) -> Engine:
         """Creates a connection engine using a database
         connection string.
         Return:
             Connection engine.
         """
-
         engine = create_engine(self.__connection_string)
         return engine
 
+    def get_engine(self) -> Engine:
+        """Get connection engine.
+        Return:
+            Connection engine.
+        """
+        return self.__engine
+
     def __enter__(self):
-        engine = create_engine(self.__connection_string)
         session_maker = sessionmaker()
         self.session = session_maker(  # pylint: disable=attribute-defined-outside-init
-            autocommit=False, autoflush=False, bind=engine
+            autocommit=False, autoflush=False, bind=self.__engine
         )
         return self
 
--- backend/tests/infra/test_db_config.py
+++ backend/tests/infra/test_db_config.py
@@ -1,4 +1,11 @@
-from app.infra.config.db_config import DB_HOST, DB_NAME, DB_PASSWORD, DB_USER
+from app.infra.config.db_config import (
+    DB_DRIVER,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USER,
+)
 from sqlalchemy.engine import Engine
 
 
@@ -13,15 +20,15 @@ def test_database_connection_string_text_information(engine: Engine):
     # Arrange
     actual_connection_string = engine.url
     expected_connection_string: list = [
-        "postgresql+psycopg2",
+        DB_DRIVER,
         DB_USER,
         DB_PASSWORD,
         DB_HOST,
-        5432,
+        DB_PORT,
         DB_NAME,
     ]
     expected_full_connection_string: str = (
-        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+        f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
 
     # Assert
@@ -30,6 +37,6 @@ def test_database_connection_string_text_information(engine: Engine):
     assert actual_connection_string[1] == expected_connection_string[1]
     assert actual_connection_string[2] == expected_connection_string[2]
     assert actual_connection_string[3] == expected_connection_string[3]
-    assert actual_connection_string[4] == expected_connection_string[4]
+    assert str(actual_connection_string[4]) == expected_connection_string[4]
     assert actual_connection_string[5] == expected_connection_string[5]
     assert str(actual_connection_string) == expected_full_connection_string
